f9bf03634883ee982dc20cf98fa9ed29f7bb4164
DilshatOsmanov <DilshatOsmanov@users.noreply.github.com>
2024-10-17 13:32:03 +0000

    deploy: c175885a1b991eb7f9c0936933506d4818bd7486

--- assets/user-j5bfa_08.js
+++
@@ -1,1 +0,0 @@
-import{h as e}from"./index-BtuXidbw.js";const a=async s=>await e.post("/users",s),o=async s=>await e.put("/auth/reset-password",s),n=async s=>await e.post("/auth/forgot-password",s),p=async s=>await e.get("/users/verify",{params:s}),c=async s=>await e.get("/oauth/auth/google",{params:s});export{a,n as c,c as g,o as r,p as v};
