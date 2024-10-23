35caf7b62b106ba756e005fcba2c46c6fd4b504f
DilshatOsmanov <DilshatOsmanov@users.noreply.github.com>
2024-10-23 05:00:04 +0000

    deploy: 4da5f07b1dd100f081d7860265e93ead918bff5e

---
+++ assets/user-D5kuJ4o8.js
@@ -0,0 +1,1 @@
+import{x as e}from"./index-CyAZYabP.js";const a=async s=>await e.post("/users",s),o=async s=>await e.put("/auth/reset-password",s),n=async s=>await e.post("/auth/forgot-password",s),p=async s=>await e.get("/users/verify",{params:s}),c=async s=>await e.get("/oauth/auth/google",{params:s});export{a,n as c,c as g,o as r,p as v};
