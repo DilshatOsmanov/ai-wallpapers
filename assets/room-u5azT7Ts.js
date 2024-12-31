c48977a8aaa6bb32195476133816a9bbbf93ae2e
DilshatOsmanov <DilshatOsmanov@users.noreply.github.com>
2024-12-31 07:42:03 +0000

    deploy: 041ce7e61e7dee57fff306ea399bf5e84e74812d

--- assets/room-u5azT7Ts.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./index-BpHBTNSw.js";const r=async()=>await e.get("/rooms/view_room/"),a=async o=>await e.post("/rooms/create_room",o,{headers:{"Content-Type":"multipart/form-data"}});export{a as c,r as g};
